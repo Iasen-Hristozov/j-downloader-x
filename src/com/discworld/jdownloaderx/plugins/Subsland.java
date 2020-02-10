a9cbdc92ebe585edea4f61f07dce5e888729737b
Iasen-Hristozov <iasen.hristozov@gmail.com>
2020-02-10 16:26:24 +0200

    Subsland plugin fixed.

--- src/com/discworld/jdownloaderx/plugins/Subsland.java
+++ src/com/discworld/jdownloaderx/plugins/Subsland.java
@@ -12,8 +12,8 @@ public class Subsland extends Plugin
    private final static String DOMAIN = "subsland.com";
    
    private final static Pattern ptnTitle = Pattern.compile("<TITLE>subsland.com - \u0421\u0443\u0431\u0442\u0438\u0442\u0440\u0438 (.+?)</TITLE>"),
-                                ptnURL = Pattern.compile("((http(s)?://)?subsland.com/subtitles/.+?\\.html)"),
-                                ptnFileURL = Pattern.compile("((http(s)?://)?subsland\\.com/downloadsubtitles/(.+?)(\\.rar)|(\\.zip))");
+                                ptnURL = Pattern.compile("((http(s)?:\\/\\/)?subsland\\.com\\/subtitles\\/.+?\\.html)"),
+                                ptnFileURL = Pattern.compile("((http(s)?:\\/\\/)?subsland\\.com\\/downloadsubtitles\\/(.+?)((\\.rar)|(\\.zip)))");
    
    static
    {
